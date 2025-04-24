c9806b7ed8c24823ab1b79721d1da341ed918859
Hanliang-Xu <113558259+Hanliang-Xu@users.noreply.github.com>
2025-04-24 13:53:35 -0400

    revised metric for annual mean return

--- stats_utils.py
+++ stats_utils.py
@@ -1,13 +1,6 @@
 import numpy as np
 import pandas as pd
 
-def compute_cagr(portfolio_values, start_date, end_date):
-    # Compute CAGR using start and end values and the time period in years
-    start_value = portfolio_values.loc[start_date]
-    end_value = portfolio_values.loc[end_date]
-    num_years = (pd.to_datetime(end_date) - pd.to_datetime(start_date)).days / 365.25
-    return (end_value / start_value) ** (1 / num_years) - 1
-
 def compute_sharpe_ratio(portfolio_values, risk_free_rate=0.0):
     # Assuming portfolio_values is a pandas Series of daily portfolio value
     daily_returns = portfolio_values.pct_change().dropna()
